4541364ec50dfa6ea8397183f76e8e8c015ebe6d
github username <github email address>
2020-05-12 03:22:15 -0700

    pokemon api, pokemon cards built

---
+++ build/precache-manifest.c485945ce31b4b8acdd443b90110bad8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be877bdd001b431df880b6d5133a19e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89d603910670a957aee3",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a9dc0c51c0d5932e7fc1",
+    "url": "/static/css/main.854a9583.chunk.css"
+  },
+  {
+    "revision": "89d603910670a957aee3",
+    "url": "/static/js/2.29355839.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.29355839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9dc0c51c0d5932e7fc1",
+    "url": "/static/js/main.8ee61738.chunk.js"
+  },
+  {
+    "revision": "db00ea0467903f548a1b",
+    "url": "/static/js/runtime-main.8a8a3104.js"
+  },
+  {
+    "revision": "8cb992dcc99c10ec1b72c5f5f5dec7b7",
+    "url": "/static/media/spinner.8cb992dc.gif"
+  }
+]);
